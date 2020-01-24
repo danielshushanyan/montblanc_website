bf9b82e1faccd283fa9aaf92de558b148bd1a6f5
im_daniel <shushanyandaniel@gmail.com>
2020-01-24 13:30:45 +0400

    production popup

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,10 @@ import './header';
 import './video-controler';
 import './mouse-catch';
 import './360image';
+import './move-products';
 
 $(function () {
-	if(window.innerWidth > 1024) new Cursor();
+	if(window.innerWidth > 768) new Cursor();
 	$('.circle-cursor--inner').css('z-index','11000');
 	$('.circle-cursor--outer').css('z-index','12000');
 	$('.circle-cursor--drag').css('z-index','12000');
